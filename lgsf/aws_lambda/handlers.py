c64ba8348f8671ecc81ac4fb313f6dc191b2f328
GeoWill <roper.william@gmail.com>
2021-08-11 13:16:30 +0100

    Define ScraperWorker function

--- lgsf/aws_lambda/handlers.py
+++ lgsf/aws_lambda/handlers.py
@@ -9,6 +9,22 @@ from lgsf.councillors.commands import Command
 from lgsf.path_utils import load_scraper
 
 
+def scraper_worker_handler(event, context):
+    print(f"EVENT: {event}")
+    message = json.loads(event["Records"][0]["body"])
+    council = message["council"]
+    command_name = message["scraper_type"]
+    scraper_cls = load_scraper(council, command_name)
+    options = {"council": council, "verbose": True, "aws_lambda": True}
+    console = Console(file=sys.stdout)
+    scraper = scraper_cls(options, console)
+    try:
+        scraper.run()
+    except Exception as e:
+        scraper.console.log(e)
+        scraper.delete_branch()
+
+
 def queue_builder_handler(event, context):
     councillors_command = Command(argv=["", "--all-councils"], stdout=sys.stdout)
     councillors_command.options = {"all_councils": True, "exclude_missing": True}
