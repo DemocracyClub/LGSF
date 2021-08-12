85fef612ee6a49fea978392eb441bce1c7937ad9
GeoWill <roper.william@gmail.com>
2021-08-12 17:45:05 +0100

    Run black on all the scrapers

--- scrapers/NLN-north-lincolnshire/councillors.py
+++ scrapers/NLN-north-lincolnshire/councillors.py
@@ -3,4 +3,3 @@ from lgsf.councillors.scrapers import ModGovCouncillorScraper
 
 class Scraper(ModGovCouncillorScraper):
     base_url = "http://northlincolnshire.moderngov.co.uk"
-
--- scrapers/OLD-oldham/councillors.py
+++ scrapers/OLD-oldham/councillors.py
@@ -3,4 +3,3 @@ from lgsf.councillors.scrapers import ModGovCouncillorScraper
 
 class Scraper(ModGovCouncillorScraper):
     base_url = "http://committees.oldham.gov.uk"
-
