756ba64aa7dbe252fe9b2d10c82aef8b477167ec
GeoWill <roper.william@gmail.com>
2021-08-27 18:12:24 +0100

    Disable a bunch of broken scrapers

--- scrapers/CBF-central-bedfordshire/councillors.py
+++ scrapers/CBF-central-bedfordshire/councillors.py
@@ -3,3 +3,4 @@ from lgsf.councillors.scrapers import ModGovCouncillorScraper
 
 class Scraper(ModGovCouncillorScraper):
     base_url = "http://centralbeds.moderngov.co.uk"
+    disabled = True
--- scrapers/WEY-weymouth-and-portland/councillors.py
+++ scrapers/WEY-weymouth-and-portland/councillors.py
@@ -3,3 +3,4 @@ from lgsf.councillors.scrapers import ModGovCouncillorScraper
 
 class Scraper(ModGovCouncillorScraper):
     base_url = "http://moderngovdcp.dorsetforyou.gov.uk"
+    disabled = True
