fdd4b17c95671bfe997fd04e3bd8e630d39cab76
GeoWill <roper.william@gmail.com>
2021-11-25 13:58:19 +0000

    Add a RunLog class

---
+++ lgsf/aws_lambda/run_log.py
@@ -0,0 +1,18 @@
+import json
+from dataclasses import dataclass, asdict
+import datetime
+
+
+@dataclass
+class RunLog:
+    """Class for keeping track of a single run of a scraper."""
+
+    start: datetime.datetime = 0
+    end: datetime.datetime = 0
+    duration: datetime.timedelta = 0
+    log: str = ""
+    error: str = ""
+
+    @property
+    def as_json(self) -> str:
+        return json.dumps(asdict(self), default=str)
