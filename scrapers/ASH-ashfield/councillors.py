5f207f69457760d0462c5368e395b26dc12737a8
Sym Roe <sym.roe@talusdesign.co.uk>
2022-04-21 22:52:43 +0100

    Fix ASH

--- scrapers/ASH-ashfield/councillors.py
+++ scrapers/ASH-ashfield/councillors.py
@@ -2,4 +2,4 @@ from lgsf.councillors.scrapers import ModGovCouncillorScraper
 
 
 class Scraper(ModGovCouncillorScraper):
-    base_url = "https://democracy.ashfield-dc.gov.uk"
+    base_url = "https://democracy.ashfield.gov.uk"
