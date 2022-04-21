8d4ad45bc1a8803c119e315a1aae313837ccc5ca
Sym Roe <sym.roe@talusdesign.co.uk>
2022-04-21 21:40:02 +0100

    Disable GLO

--- scrapers/GLO-gloucester/councillors.py
+++ scrapers/GLO-gloucester/councillors.py
@@ -2,4 +2,6 @@ from lgsf.councillors.scrapers import ModGovCouncillorScraper
 
 
 class Scraper(ModGovCouncillorScraper):
+    # https://github.com/DemocracyClub/LGSF/issues/79
+    disabled = True
     base_url = "http://democracy.gloucester.gov.uk"
