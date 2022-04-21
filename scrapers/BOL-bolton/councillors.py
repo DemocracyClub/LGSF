f2c60a6ab7a877efb7aca41434435bd158af864e
Sym Roe <sym.roe@talusdesign.co.uk>
2022-04-21 23:01:45 +0100

    Disable BOL

--- scrapers/BOL-bolton/councillors.py
+++ scrapers/BOL-bolton/councillors.py
@@ -2,4 +2,6 @@ from lgsf.councillors.scrapers import CMISCouncillorScraper
 
 
 class Scraper(CMISCouncillorScraper):
+    # https://github.com/DemocracyClub/LGSF/issues/80
+    disabled = True
     base_url = "http://democracy.bolton.gov.uk/cmis5/People.aspx"
