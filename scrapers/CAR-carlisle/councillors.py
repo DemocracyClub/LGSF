f5ad6a3c47fe01443edbd0d749a043faccd12749
Sym Roe <sym.roe@talusdesign.co.uk>
2022-04-21 23:04:19 +0100

    Fix CAR

--- scrapers/CAR-carlisle/councillors.py
+++ scrapers/CAR-carlisle/councillors.py
@@ -2,4 +2,4 @@ from lgsf.councillors.scrapers import CMISCouncillorScraper
 
 
 class Scraper(CMISCouncillorScraper):
-    base_url = "http://cmis.carlisle.gov.uk/cmis/CarlisleCityCouncillors/tabid/62/ScreenMode/Alphabetical/Default.aspx"
+    base_url = "https://carlisle.cmis.uk.com/CarlisleCityCouncillors.aspx"
