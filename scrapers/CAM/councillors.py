6f335f99afab1b9621720b4769dec79c6541d49b
Sym Roe <sym.roe@talusdesign.co.uk>
2022-04-21 23:01:58 +0100

    Fix CAM

--- scrapers/CAM/councillors.py
+++ scrapers/CAM/councillors.py
@@ -2,5 +2,5 @@ from lgsf.councillors.scrapers import CMISCouncillorScraper
 
 
 class Scraper(CMISCouncillorScraper):
-    base_url = "https://cmis.cambridgeshire.gov.uk/ccc_live/Councillors.aspx"
+    base_url = "https://cambridgeshire.cmis.uk.com/ccc_live/Councillors.aspx"
     division_text = "Division:"
