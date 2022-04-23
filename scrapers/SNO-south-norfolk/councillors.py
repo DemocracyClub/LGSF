2cabe199e551158754fb5339922b54f240d08c71
Sym Roe <sym.roe@talusdesign.co.uk>
2022-04-23 15:22:39 +0100

    Fix SNO

--- scrapers/SNO-south-norfolk/councillors.py
+++ scrapers/SNO-south-norfolk/councillors.py
@@ -1,28 +1,32 @@
-from lgsf.councillors.scrapers import PagedHTMLCouncillorScraper
+import re
+from urllib.parse import urljoin
+
+from lgsf.councillors import SkipCouncillorException
+from lgsf.councillors.scrapers import HTMLCouncillorScraper
 
 
-class Scraper(PagedHTMLCouncillorScraper):
-    base_url = "https://www.south-norfolk.gov.uk/about-us/councillors-and-committees/your-councillors"
+class Scraper(HTMLCouncillorScraper):
+    base_url = "https://www.southnorfolkandbroadland.gov.uk/directory/3/south-norfolk-councillor-directory/category/11"
     list_page = {
-        "container_css_selector": ".listing",
-        "councillor_css_selector": ".media",
-        "next_page_css_selector": ".pager__item--next",
+        "container_css_selector": "ul.list--record",
+        "councillor_css_selector": ".list__item",
     }
 
-    def get_next_link(self, soup):
-        url = super().get_next_link(soup)
-        if url:
-            return self.base_url + url
-
     def get_single_councillor(self, councillor_html):
-        url = (
-            "https://www.south-norfolk.gov.uk"
-            + councillor_html.select(".councillor-sub-details--top")[0].a["href"]
+        url = urljoin(
+            self.base_url,
+            councillor_html.a["href"],
         )
         soup = self.get_page(url)
         name = soup.h1.get_text(strip=True)
-        party = soup.find(text="Party").findNext("div").get_text(strip=True)
-        division = soup.find(text="Ward").findNext("div").get_text(strip=True)
+        if name == "Vacancy":
+            raise SkipCouncillorException("Vacancy")
+        names = name.split(", ")
+        name = f"{names[1]} {names[0]}"
+        party = soup.find(text=re.compile("Party")).findNext("dd").get_text(strip=True)
+        division = (
+            soup.find(text=re.compile("Ward")).findNext("dd").get_text(strip=True)
+        )
 
         # Find a way to call this and return the councillor object
         councillor = self.add_councillor(
@@ -31,8 +35,8 @@ class Scraper(PagedHTMLCouncillorScraper):
 
         councillor.email = soup.select("a[href^=mailto]")[0].get_text(strip=True)
 
-        councillor.photo_url = (
-            "https://www.south-norfolk.gov.uk"
-            + soup.find("img", {"class": "image-style-councillor-profile"})["src"]
+        councillor.photo_url = urljoin(
+            "https://www.south-norfolk.gov.uk",
+            soup.select_one(".definition__content--image").img["src"],
         )
         return councillor
