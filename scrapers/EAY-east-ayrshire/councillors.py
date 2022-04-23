37d86c740cc9424583b7e2851e45a505642468c4
Sym Roe <sym.roe@talusdesign.co.uk>
2022-04-23 22:39:57 +0100

    Scraper for EAY

---
+++ scrapers/EAY-east-ayrshire/councillors.py
@@ -0,0 +1,43 @@
+import re
+from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup
+
+from lgsf.councillors.scrapers import HTMLCouncillorScraper
+
+
+class Scraper(HTMLCouncillorScraper):
+    disabled = False
+    base_url = "https://www.east-ayrshire.gov.uk/CouncilAndGovernment/About-the-Council/Councillors-and-Provost/YourCouncillor.aspx"
+    list_page = {
+        "container_css_selector": ".main-article",
+        "councillor_css_selector": ".col-md-3",
+    }
+
+    def get_single_councillor(self, councillor_html):
+        url = urljoin(self.base_url, councillor_html.select_one("a")["href"])
+        soup = self.get_page(url)
+        name = councillor_html.select_one("a").get_text(strip=True)
+
+        content_box = (
+            soup.find("span", text=re.compile("Contact Information"))
+            .find_parent()
+            .get_text(strip=True, separator="\n")
+            .splitlines()
+        )
+
+        party = content_box[2]
+        division = soup.h2.get_text(strip=True).split(":")[-1].strip()
+        councillor = self.add_councillor(
+            url,
+            identifier=url,
+            name=name,
+            party=party,
+            division=division,
+        )
+        councillor.email = soup.select_one("a[href^=mailto]")["href"]
+        councillor.photo_url = urljoin(
+            self.base_url, soup.find("img", src=re.compile("Councillors"))["src"]
+        )
+
+        return councillor
