03ab84654632ff0d45e26aac5eb969d8e95e0e7a
Sym Roe <sym.roe@talusdesign.co.uk>
2022-04-23 20:57:31 +0100

    Scraper for CLK

---
+++ scrapers/CLK-clackmannanshire/councillors.py
@@ -0,0 +1,44 @@
+import re
+from urllib.parse import urljoin
+
+from councillors.scrapers import HTMLCouncillorScraper
+
+
+class Scraper(HTMLCouncillorScraper):
+    base_url = "https://www.clacks.gov.uk/council/wards/"
+
+    list_page = {
+        "container_css_selector": "div.col-md-14",
+        "councillor_css_selector": ".col-sm-3",
+    }
+
+    def get_single_councillor(self, councillor_html):
+        url = urljoin(self.base_url, councillor_html.a["href"])
+        soup = self.get_page(url)
+        name = (
+            soup.select_one("div.col-md-14")
+            .h2.get_text(strip=True)
+            .replace("Councillor", "")
+        )
+        ward = (
+            soup.find("strong", text=re.compile("Ward:"))
+            .find_parent("li")
+            .get_text(strip=True)
+            .replace("Ward:", "")
+            .strip()
+        )
+        party = councillor_html.h5.get_text(strip=True)
+
+        councillor = self.add_councillor(
+            url,
+            identifier=url,
+            name=name,
+            party=party,
+            division=ward,
+        )
+        councillor.email = soup.select_one("a.email").getText(strip=True)
+        if soup.select_one("img.right"):
+            councillor.photo_url = urljoin(
+                self.base_url, soup.select_one("img.right")["src"]
+            )
+        return councillor
