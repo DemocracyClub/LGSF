c91740415825edcc1ff78e7f1112580d479e4c39
Sym Roe <sym.roe@talusdesign.co.uk>
2022-04-23 14:30:48 +0100

    Fix LIC

--- scrapers/LIC-city-of-lincoln/councillors.py
+++ scrapers/LIC-city-of-lincoln/councillors.py
@@ -2,4 +2,5 @@ from lgsf.councillors.scrapers import ModGovCouncillorScraper
 
 
 class Scraper(ModGovCouncillorScraper):
-    base_url = "http://democratic.lincoln.gov.uk"
+    base_url = "https://democratic.lincoln.gov.uk"
+    verify_requests = False
