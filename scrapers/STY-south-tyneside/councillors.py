c3b824e8bac1dfbb27e54bbba729d42e4bf31813
Sym Roe <sym.roe@talusdesign.co.uk>
2023-04-22 11:59:56 +0100

    Update metadata

---
+++ scrapers/STY-south-tyneside/councillors.py
@@ -0,0 +1,19 @@
+from lgsf.councillors.scrapers import HTMLCouncillorScraper
+
+
+class Scraper(HTMLCouncillorScraper):
+    base_url = "https://www.southtyneside.gov.uk"
+
+    def get_redirect_url_from_page(self, soup):
+        pass
+
+    def get_councillors(self):
+        base_soup = self.get_page(
+            f"{self.base_url}/article/60208/Find-your-councillors"
+        )
+        import ipdb
+
+        ipdb.set_trace()
+
+    def get_single_councillor(self):
+        pass
