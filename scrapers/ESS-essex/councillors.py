51a78f2cea2fc5f5cf421ac22d3f5c4fe03df30d
Sym Roe <sym.roe@talusdesign.co.uk>
2023-04-22 12:27:37 +0100

    Fix ESS

--- scrapers/ESS-essex/councillors.py
+++ scrapers/ESS-essex/councillors.py
@@ -2,5 +2,5 @@ from lgsf.councillors.scrapers import CMISCouncillorScraper
 
 
 class Scraper(CMISCouncillorScraper):
-    base_url = "http://cmis.essexcc.gov.uk/EssexCmis5/Councillors.aspx"
+    base_url = "http://cmis.essex.gov.uk/EssexCmis5/Councillors.aspx"
     division_text = "Division:"
