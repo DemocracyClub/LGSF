10f2c3bbaec4d907a0e5e5839fbfbf527e840be0
Sym Roe <sym.roe@talusdesign.co.uk>
2023-09-02 20:10:47 +0100

    Fix TUN

--- scrapers/TUN-tunbridge-wells/councillors.py
+++ scrapers/TUN-tunbridge-wells/councillors.py
@@ -2,4 +2,4 @@ from lgsf.councillors.scrapers import ModGovCouncillorScraper
 
 
 class Scraper(ModGovCouncillorScraper):
-    base_url = "http://democracy.tunbridgewells.gov.uk"
+    base_url = "https://democracy.tunbridgewells.gov.uk"
