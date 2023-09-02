9f603d731e7f2bf9c2d1c05bbea431562ebe134c
Sym Roe <sym.roe@talusdesign.co.uk>
2023-09-02 20:19:13 +0100

    Fix BDF

--- scrapers/BDF-bedford/councillors.py
+++ scrapers/BDF-bedford/councillors.py
@@ -2,4 +2,4 @@ from lgsf.councillors.scrapers import ModGovCouncillorScraper
 
 
 class Scraper(ModGovCouncillorScraper):
-    base_url = "http://www.councillorsupport.bedford.gov.uk"
+    base_url = "https://councillorsupport.bedford.gov.uk"
