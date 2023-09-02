9da28b6cc19addf1f78749adba3f03267c06fed0
Sym Roe <sym.roe@talusdesign.co.uk>
2023-09-02 19:40:36 +0100

    Enable GLO
    Fixes #79

--- scrapers/GLO-gloucester/councillors.py
+++ scrapers/GLO-gloucester/councillors.py
@@ -2,6 +2,4 @@ from lgsf.councillors.scrapers import ModGovCouncillorScraper
 
 
 class Scraper(ModGovCouncillorScraper):
-    # https://github.com/DemocracyClub/LGSF/issues/79
-    disabled = True
     base_url = "http://democracy.gloucester.gov.uk"
