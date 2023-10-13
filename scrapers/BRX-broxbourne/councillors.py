e1cb4be97ebdc05a00363baf9e840e42808d9ec5
Sym Roe <sym.roe@talusdesign.co.uk>
2023-10-13 11:31:01 +0100

    Fix BRX (move to ModGov)

--- scrapers/BRX-broxbourne/councillors.py
+++ scrapers/BRX-broxbourne/councillors.py
@@ -1,34 +1,6 @@
 from urllib.parse import urljoin
-from lgsf.councillors.scrapers import HTMLCouncillorScraper
+from lgsf.councillors.scrapers import ModGovCouncillorScraper
 
 
-class Scraper(HTMLCouncillorScraper):
-    disabled = False
-    base_url = "https://www.broxbourne.gov.uk/councillors"
-    list_page = {
-        "container_css_selector": ".container .page-content .list.list--listing",
-        "councillor_css_selector": ".list__item",
-    }
-
-    def get_single_councillor(self, councillor_html):
-        name = councillor_html.h2.get_text(strip=True)
-        url = urljoin(self.base_url, councillor_html.findAll("a")[0]["href"])
-        identifier = url.split("/")[-2]
-        councillor_html = self.get_page(url)
-        division = councillor_html.find(text="Ward:").next.strip()
-        party = councillor_html.find(text="Party:").next.strip()
-        councillor = self.add_councillor(
-            url, identifier=identifier, name=name, party=party, division=division
-        )
-        try:
-            councillor.email = (
-                councillor_html.find(text="Email:").findNext("a").get_text(strip=True)
-            )
-        except AttributeError:
-            pass
-
-        image_url = councillor_html.find_all("img", class_="image image--feature")[0][
-            "src"
-        ]
-        councillor.photo_url = urljoin(self.base_url, image_url)
-        return councillor
+class Scraper(ModGovCouncillorScraper):
+    base_url = "https://broxbourne.moderngov.co.uk"
