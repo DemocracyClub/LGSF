a19a36fbe1ff63a0456e26cd89d4ab9cd999bbf6
Sym Roe <sym.roe@talusdesign.co.uk>
2023-10-13 11:38:50 +0100

    Fix CWY

--- scrapers/CWY-conwy/councillors.py
+++ scrapers/CWY-conwy/councillors.py
@@ -2,4 +2,4 @@ from lgsf.councillors.scrapers import ModGovCouncillorScraper
 
 
 class Scraper(ModGovCouncillorScraper):
-    base_url = "http://modgoveng.conwy.gov.uk"
+    base_url = "https://modgovpub-eng.secure.conwy.gov.uk"
