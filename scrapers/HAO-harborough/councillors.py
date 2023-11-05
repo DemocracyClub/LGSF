4534396bc12e680b02b6278e2aba04beed507b87
Sym Roe <sym.roe@talusdesign.co.uk>
2023-11-05 09:59:28 +0000

    Fix HAO

--- scrapers/HAO-harborough/councillors.py
+++ scrapers/HAO-harborough/councillors.py
@@ -2,4 +2,5 @@ from lgsf.councillors.scrapers import CMISCouncillorScraper
 
 
 class Scraper(CMISCouncillorScraper):
+    verify_requests = False
     base_url = "https://cmis.harborough.gov.uk/cmis5/Councillors.aspx"
