c6436423e9012cd2b8921f1cc5f607db2162c888
Sym Roe <sym.roe@talusdesign.co.uk>
2023-11-05 10:03:09 +0000

    Fix HNS

--- scrapers/HNS-hounslow/councillors.py
+++ scrapers/HNS-hounslow/councillors.py
@@ -2,4 +2,5 @@ from lgsf.councillors.scrapers import ModGovCouncillorScraper
 
 
 class Scraper(ModGovCouncillorScraper):
+    verify_requests = False
     base_url = "https://democraticservices.hounslow.gov.uk"
