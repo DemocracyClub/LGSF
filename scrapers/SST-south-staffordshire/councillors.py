91a5afd7ffb177253d4a714aeed7374050a2f47d
Sym Roe <sym.roe@talusdesign.co.uk>
2023-11-19 17:07:13 +0000

    Fix SST

--- scrapers/SST-south-staffordshire/councillors.py
+++ scrapers/SST-south-staffordshire/councillors.py
@@ -2,4 +2,4 @@ from lgsf.councillors.scrapers import CMISCouncillorScraper
 
 
 class Scraper(CMISCouncillorScraper):
-    base_url = "https://services.sstaffs.gov.uk/cmis/Councillors.aspx"
+    base_url = "https://southstaffs.cmis.uk.com/Councillors.aspx"
