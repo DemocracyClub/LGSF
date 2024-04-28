039e9a7a621a955caa4d50042a60cc2ea6bd0e69
Sym Roe <sym.roe@talusdesign.co.uk>
2024-04-28 13:10:09 +0100

    Enable disabled scrapers
    These servers previously timed out

--- scrapers/MAL-maldon/councillors.py
+++ scrapers/MAL-maldon/councillors.py
@@ -3,4 +3,3 @@ from lgsf.councillors.scrapers import ModGovCouncillorScraper
 
 class Scraper(ModGovCouncillorScraper):
     base_url = "https://democracy.maldon.gov.uk"
-    disabled = True
--- scrapers/SPE-spelthorne/councillors.py
+++ scrapers/SPE-spelthorne/councillors.py
@@ -3,6 +3,3 @@ from lgsf.councillors.scrapers import ModGovCouncillorScraper
 
 class Scraper(ModGovCouncillorScraper):
     base_url = "https://democracy.spelthorne.gov.uk"
-
-    # Server timeout 2024-04-27
-    disabled = True
