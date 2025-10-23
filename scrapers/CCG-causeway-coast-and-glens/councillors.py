08f22517abe03e83aa1e3a2839c8b2f749709cc9
symroe <symroe@users.noreply.github.com>
2025-10-23 21:53:01 +0100

    Fix CCG

--- scrapers/CCG-causeway-coast-and-glens/councillors.py
+++ scrapers/CCG-causeway-coast-and-glens/councillors.py
@@ -6,24 +6,24 @@ from lgsf.councillors.scrapers import HTMLCouncillorScraper
 
 class Scraper(HTMLCouncillorScraper):
     list_page = {
-        "container_css_selector": ".councillor-list",
-        "councillor_css_selector": ".item",
+        "container_css_selector": "#board",
+        "councillor_css_selector": "a.no-underline",
     }
 
     def get_single_councillor(self, councillor_html):
-        councillor_url = councillor_html.select_one("a.btn")["href"]
+        councillor_url = councillor_html["href"]
         url = urljoin(self.base_url, councillor_url)
         soup = self.get_page(url)
         name = soup.h1.get_text(strip=True)
 
         party = (
-            soup.find("strong", text=re.compile("Party:"))
+            soup.find("span", text=re.compile("Party:"))
             .find_parent("p")
             .get_text(strip=True)
             .replace("Party:", "")
         )
         division = (
-            soup.find("strong", text=re.compile("District Electoral Area:"))
+            soup.find("span", text=re.compile("District Electoral Area:"))
             .find_parent("p")
             .get_text(strip=True)
             .replace("District Electoral Area:", "")
@@ -32,7 +32,7 @@ class Scraper(HTMLCouncillorScraper):
         councillor = self.add_councillor(
             url, identifier=url, name=name, party=party, division=division
         )
-        container = soup.select_one(".page-content-section")
+        container = soup.select_one("main")
 
         councillor.email = container.select_one("a[href^=mailto]")["href"].replace(
             "mailto:", ""
