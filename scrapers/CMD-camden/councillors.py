811e18a7e23b7f232af195f9df57808735495570
symroe <symroe@users.noreply.github.com>
2025-11-05 21:45:21 +0000

    Remove curl_cffi

--- scrapers/CMD-camden/councillors.py
+++ scrapers/CMD-camden/councillors.py
@@ -2,7 +2,4 @@ from lgsf.councillors.scrapers import ModGovCouncillorScraper
 
 
 class Scraper(ModGovCouncillorScraper):
-    def get(self, url, extra_headers=None):
-        from curl_cffi import requests
-
-        return requests.get(url, impersonate="chrome")
+    ...
