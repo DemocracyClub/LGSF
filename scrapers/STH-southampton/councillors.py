752672f686255a4b968519251eb9ab6a4754f427
symroe <symroe@users.noreply.github.com>
2026-06-04 20:03:13 +0100

    Use playwright for some scrapers

--- scrapers/STH-southampton/councillors.py
+++ scrapers/STH-southampton/councillors.py
@@ -6,7 +6,7 @@ from lgsf.councillors.scrapers import HTMLCouncillorScraper
 
 
 class Scraper(HTMLCouncillorScraper):
-    http_lib = "requests"
+    http_lib = "playwright"
 
     list_page = {
         "container_css_selector": "div.mgThumbsList",
